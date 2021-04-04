44bfddebdb854318e57694f7cd1ae401af20c4d0
Wilker Reyes <wr177836@fi365.ort.edu.uy>
2021-04-03 23:03:00 -0300

    Agregación de atributos

---
+++ TVision/Canal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVision
+{
+    class Canal
+    {
+        private int id;
+        private string nombre;
+        private List<int> resolucion = new List<int>();
+        private Boolean esMultilenguaje;
+        private int precioBase;
+    }
+}
---
+++ TVision/Paquete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVision
+{
+    class Paquete
+    {
+        private int id;
+        private string nombre;
+        private List<Canal> listaCanales;
+        private Boolean promocion;
+        private float precioBase;
+        private float precioFinal;
+}
+}
