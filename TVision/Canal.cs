969fdaba78ae8abcbafb5a178528df375a42fcf3
Wilker Reyes <wr177836@fi365.ort.edu.uy>
2021-04-04 12:29:32 -0300

    Cambio en clase Canal

--- TVision/Canal.cs
+++ TVision/Canal.cs
@@ -10,7 +10,7 @@ namespace TVision
     {
         private int id;
         private string nombre;
-        private List<int> resolucion = new List<int>();
+        private List<int> resolucion;
         private Boolean esMultilenguaje;
         private int precioBase;
     }
